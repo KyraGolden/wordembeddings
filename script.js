5a058808ac86a51856ef962b261f10167d4d432b
Kyra <kyra.goud@uzh.ch>
2019-12-29 18:15:59 +0100

    HTML angepasst, Js script angepasst

--- script.js
+++ script.js
@@ -36,3 +36,14 @@ $(document).ready(function(){
         });
     });
 });
+$(window).load(function () {  //this is the function for the help popup
+    $(".trigger_popup_fricc").click(function(){
+       $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+});
